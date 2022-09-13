b21f5a1d1dbeb2e9cb83f33f03ec35f652ed92f2
Veronika Laippala <laippala@puhti-login1.bullx>
2022-09-13 23:11:33 +0300

    adding data

--- deprecated/multilabel_transformers/setup_env.sh
+++
@@ -1,7 +0,0 @@
-mkdir logs
-
-module purge
-module load tensorflow/2.2-hvd
-python -m venv VENV-tf2.2-transformers3.4
-source VENV-tf2.2-transformers3.4/bin/activate
-python -m pip install -r requirements.txt
